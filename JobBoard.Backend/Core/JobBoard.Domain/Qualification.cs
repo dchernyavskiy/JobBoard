74e466dec71444879d9fef9745e31ae61f3b3ff9
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-11-01 21:01:04 +0200

    added domain logic

---
+++ JobBoard.Backend/Core/JobBoard.Domain/Qualification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Domain
+{
+    public class Qualification
+    {
+        public Guid Id { get; set; }
+        public string Discription { get; set; }
+        public Guid JobId { get; set; }
+        public Job Job { get; set; }
+    }
+}
