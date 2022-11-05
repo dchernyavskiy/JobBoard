2a95ea45aa858018ce8056a871c73b6595dea097
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-11-05 19:33:17 +0200

    added redirecting to client. some changes in domain logic

--- JobBoard.Backend/Presentation/JobBoard.WebApi/Migrations/20221105165238_Initial3.Designer.cs
+++ JobBoard.Backend/Presentation/JobBoard.WebApi/Migrations/20221105165238_Initial3.Designer.cs
@@ -4,6 +4,7 @@ using JobBoard.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JobBoard.WebApi.Migrations
 {
     [DbContext(typeof(JobBoardDbContext))]
-    partial class JobBoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221105165238_Initial3")]
+    partial class Initial3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
