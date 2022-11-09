09afc03eccf28fc133c3021511931c9a4e6d5e3d
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-11-09 23:44:19 +0200

    added oidc client to front

--- JobBoard.Frontend/job-board/src/app/app.module.ts
+++ JobBoard.Frontend/job-board/src/app/app.module.ts
@@ -1,25 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
-import { NavbarStyleThreeComponent } from './components/common/navbar-style-three/navbar-style-three.component';
 import { FooterStyleTwoComponent } from './components/common/footer-style-two/footer-style-two.component';
-import { JobsComponent } from './components/pages/jobs/jobs.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeThreeComponent,
-    NavbarStyleThreeComponent,
-    FooterStyleTwoComponent,
-    JobsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeThreeComponent,
+        FooterStyleTwoComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AuthModule.forRoot({
+            config: {
+                authority: 'https://localhost:5002',
+                redirectUrl: window.location.origin,
+                postLogoutRedirectUri: window.location.origin,
+                clientId: 'job-board-web-app',
+                scope: 'openid profile JobBoardWebApi',
+                responseType: 'code',
+                silentRenew: true,
+                useRefreshToken: true,
+                logLevel: LogLevel.Debug,
+            },
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
