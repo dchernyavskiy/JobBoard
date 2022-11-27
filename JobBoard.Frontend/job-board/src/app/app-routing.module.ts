f7b017ec2fc6df7dbff4aa0a49571bc7b6435797
danyloosypov <48645561+danyloosypov@users.noreply.github.com>
2022-11-27 17:19:38 +0200

    favourite jobs page. employers page. employer card.

--- JobBoard.Frontend/job-board/src/app/app-routing.module.ts
+++ JobBoard.Frontend/job-board/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeThreeComponent } from './components/home-three/home-three.component';
 import { JobsnewComponent } from './components/jobsnew/jobsnew.component';
+import { FavouriteJobsComponent } from './components/favourite-jobs/favourite-jobs.component';
+import { EmployersComponent } from './components/employers/employers.component';
+
+
 
 const routes: Routes = [
     {path: '', component: HomeThreeComponent},
-    {path: 'jobsnew', component: JobsnewComponent}
+    {path: 'jobsnew', component: JobsnewComponent},
+    {path: 'favourite-jobs', component: FavouriteJobsComponent},
+    {path: 'employers', component: EmployersComponent}
+
+
     
 ];
 
---
+++ JobBoard.Frontend/job-board/src/app/components/employer-card/employer-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-card',
+  templateUrl: './employer-card.component.html',
+  styleUrls: ['./employer-card.component.scss']
+})
+export class EmployerCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
