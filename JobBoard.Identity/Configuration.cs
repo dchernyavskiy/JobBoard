dc6d64ed317399b875bb3c33a838be92afad2722
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-12-06 18:04:54 +0200

    removed extra symbols in using statement

--- JobBoard.Identity/Configuration.cs
+++ JobBoard.Identity/Configuration.cs
@@ -1,11 +1,6 @@
-﻿воддуusing IdentityModel;
+﻿using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JobBoard.Identity
 {
