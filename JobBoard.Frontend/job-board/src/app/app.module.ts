e01e33f54d0b9bcaf368875b0e7cdfee290d5c86
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-12-13 22:11:27 +0200

    bad admin imp

--- JobBoard.Frontend/job-board/src/app/app.module.ts
+++ JobBoard.Frontend/job-board/src/app/app.module.ts
@@ -19,12 +19,25 @@ import { PostAJobComponent } from './components/post-a-job/post-a-job.component'
 import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 import { EmployersDetailsComponent } from './components/employers-details/employers-details.component';
-import { DefaultLayoutComponent } from './components/layouts/default-layout/default-layout.component'
+import { DefaultLayoutComponent } from './components/layouts/default-layout/default-layout.component';
+import { AdminLayoutComponent } from './components/layouts/admin-layout/admin-layout.component'
+import { SidebarModule, ToolbarModule } from "@syncfusion/ej2-angular-navigations";
+import { ListViewModule } from "@syncfusion/ej2-angular-lists";
+import { MatButtonModule, MatDividerModule, MatIconModule, MatSidenavModule, MatToolbarModule } from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 @NgModule({
-  declarations: [AppComponent, HomeThreeComponent, JobDetailsComponent, FooterStyleTwoComponent, JobCardComponent, JobsnewComponent, FavouriteJobsComponent, EmployersComponent, EmployerCardComponent, DashboardComponent, PostAJobComponent, EmployersDetailsComponent, DefaultLayoutComponent  ],
+  declarations: [AppComponent, HomeThreeComponent, JobDetailsComponent, FooterStyleTwoComponent, JobCardComponent, JobsnewComponent, FavouriteJobsComponent, EmployersComponent, EmployerCardComponent, DashboardComponent, PostAJobComponent, EmployersDetailsComponent, DefaultLayoutComponent, AdminLayoutComponent  ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
     CommonModule,
     BrowserModule,
+    SidebarModule,
+    ListViewModule,
+    ToolbarModule,
     AppRoutingModule,
     FormsModule,
     AuthModule.forRoot({
