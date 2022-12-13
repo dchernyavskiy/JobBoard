13c25c782101781e3047cabe0c13572dcdaa1f46
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-12-13 23:13:07 +0200

    Revert "delete"
    This reverts commit ecfc97c0a61f4403c881a8e7d73cf949d9624330.

--- JobBoard.Frontend/job-board/src/app/app.module.ts
+++ JobBoard.Frontend/job-board/src/app/app.module.ts
@@ -19,13 +19,10 @@ import { PostAJobComponent } from './components/post-a-job/post-a-job.component'
 import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 import { EmployersDetailsComponent } from './components/employers-details/employers-details.component';
-import { DefaultLayoutComponent } from './components/layouts/default-layout/default-layout.component';
-import { AdminLayoutComponent } from './components/layouts/admin-layout/admin-layout.component'
-import { SidebarMenuModule } from "angular-sidebar-menu";
+import { DefaultLayoutComponent } from './components/layouts/default-layout/default-layout.component'
 @NgModule({
-  declarations: [AppComponent, HomeThreeComponent, JobDetailsComponent, FooterStyleTwoComponent, JobCardComponent, JobsnewComponent, FavouriteJobsComponent, EmployersComponent, EmployerCardComponent, DashboardComponent, PostAJobComponent, EmployersDetailsComponent, DefaultLayoutComponent, AdminLayoutComponent  ],
+  declarations: [AppComponent, HomeThreeComponent, JobDetailsComponent, FooterStyleTwoComponent, JobCardComponent, JobsnewComponent, FavouriteJobsComponent, EmployersComponent, EmployerCardComponent, DashboardComponent, PostAJobComponent, EmployersDetailsComponent, DefaultLayoutComponent  ],
   imports: [
-    SidebarMenuModule,
     CommonModule,
     BrowserModule,
     AppRoutingModule,
