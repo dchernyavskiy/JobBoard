89f25d1e2d06efbb4a81c3fa7a9f13e13b186c44
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2022-12-14 14:04:12 +0200

    added admin dashboard

--- JobBoard.Frontend/job-board/src/app/app.module.ts
+++ JobBoard.Frontend/job-board/src/app/app.module.ts
@@ -19,9 +19,10 @@ import { PostAJobComponent } from './components/post-a-job/post-a-job.component'
 import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 import { EmployersDetailsComponent } from './components/employers-details/employers-details.component';
-import { DefaultLayoutComponent } from './components/layouts/default-layout/default-layout.component'
+import { DefaultLayoutComponent } from './components/layouts/default-layout/default-layout.component';
+import { AdminLayoutComponent } from './components/layouts/admin-layout/admin-layout.component'
 @NgModule({
-  declarations: [AppComponent, HomeThreeComponent, JobDetailsComponent, FooterStyleTwoComponent, JobCardComponent, JobsnewComponent, FavouriteJobsComponent, EmployersComponent, EmployerCardComponent, DashboardComponent, PostAJobComponent, EmployersDetailsComponent, DefaultLayoutComponent  ],
+  declarations: [AppComponent, HomeThreeComponent, JobDetailsComponent, FooterStyleTwoComponent, JobCardComponent, JobsnewComponent, FavouriteJobsComponent, EmployersComponent, EmployerCardComponent, DashboardComponent, PostAJobComponent, EmployersDetailsComponent, DefaultLayoutComponent, AdminLayoutComponent  ],
   imports: [
     CommonModule,
     BrowserModule,
--- JobBoard.Identity/Models/LoginViewModel.cs
+++ JobBoard.Identity/Models/LoginViewModel.cs
@@ -11,6 +11,6 @@ namespace JobBoard.Identity.Models
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        public string ReturnUrl { get; set; }
+        public string? ReturnUrl { get; set; }
     }
 }
